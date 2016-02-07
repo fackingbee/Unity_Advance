668da48b26b72d467c24bfb6d463dd507f0b3530
fackingbee <fackingbee1983@yahoo.co.jp>
2016-02-07 16:33:31 +0900

    Photon_Import

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Cube : MonoBehaviour {
 
-	// Use this for initialization
+	//
 	void Start () {
 	
 	}
---
+++ Assets/manage.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manage : MonoBehaviour {
+
+	private bool keyLock;
+
+	// Use this for initialization
+	void Start () {
+		
+		keyLock = false;
+
+		// Photon Realtimeのサーバーへ接続→ロビーへ入室
+		PhotonNetwork.ConnectUsingSettings (null);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	// ロビーに入室
+	void OnJoinedLobby(){
+
+		// とりあえずどこかのルームへ入室
+		PhotonNetwork.JoinRandomRoom();
+
+	}
+
+
+	// ルームへ入室
+	void OnJoinedRoom(){
+
+		// 入室が完了したことを出力し、キーロックを解除
+		Debug.Log ("ルームへ入室しました");
+		keyLock = true;
+	}
+
+
+	// ルームの入室に失敗
+	void OnPhotonRandomJoinFailed(){
+
+		// 自分でルームを作成して入室
+		PhotonNetwork.CreateRoom (null);
+	}
+
+
+	void FixedUpdate(){
+
+		// 左クリックが押されたらオブジェクトを読み込み
+		if(Input.GetMouseButtonDown(0) && keyLock){
+
+			GameObject mySyncObj = PhotonNetwork.Instantiate ("Cube", new Vector3(9.0f,0f,0f),Quaternion.identity,0);
+
+			// 動きをつけるためにRigidBodyを取得し、力を加える
+			Rigidbody mySyncObjRB = mySyncObj.GetComponent<Rigidbody> ();
+
+			mySyncObjRB.isKinematic = false;
+
+			float rndPow = Random.Range (1.0f,5.0f);
+
+			mySyncObjRB.AddForce (Vector3.left * rndPow, ForceMode.Impulse);
+		}
+
+	}
+
+}
